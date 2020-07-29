ad92d62f16e4df369e37f280f90bfe900b49d9b4
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-07-29 18:59:49 +1000

    Updated debugger documentation.

--- YACE/Debugger.cs
+++ YACE/Debugger.cs
@@ -5,13 +5,22 @@ using System.Text;
 
 namespace YACE
 {
+    /// <summary>
+    /// Class for accessing emulator debug information
+    /// </summary>
     public class Debugger
     {
         private Emulator _emulator;
         private CPU _cpu;
         private Memory _memory;
 
+        /// <summary>
+        /// Occurs when a register's value is changed via the debugger.
+        /// </summary>
         public event Action RegisterValueChanged;
+        /// <summary>
+        /// Occurs when the emulator is stepped using the debugger.
+        /// </summary>
         public event Action Stepped;
 
         public Debugger(Emulator Emulator, CPU CPU,Memory Memory)
@@ -21,11 +30,19 @@ namespace YACE
             _memory = Memory;
         }
 
+        /// <summary>
+        /// Get the current Program Counter register value
+        /// </summary>
+        /// <returns></returns>
         public ushort GetProgramCounter()
         {
             return _cpu.PC;
         }
 
+        /// <summary>
+        /// Set the current Program Counter register value.
+        /// </summary>
+        /// <param name="Value"></param>
         public void SetProgramCounter(ushort Value)
         {
             if (Value > 0xFFF)
@@ -34,6 +51,11 @@ namespace YACE
             RegisterValueChanged?.Invoke();
         }
 
+        /// <summary>.
+        /// Get the value of the specified register
+        /// </summary>
+        /// <param name="RegisterIndex">The index of the register (0x0 - 0xF).</param>
+        /// <returns>The register value</returns>
         public byte GetMainRegister(int RegisterIndex)
         {
             if (RegisterIndex > 15)
@@ -41,6 +63,11 @@ namespace YACE
             return _cpu.Registers[RegisterIndex];
         }
 
+        /// <summary>
+        /// Sets the value of the specified register
+        /// </summary>
+        /// <param name="RegisterIndex">The index of the register (0x0 - 0xF).</param>
+        /// <param name="Value">The new value</param>
         public void SetMainRegister(int RegisterIndex, byte Value)
         {
             if(RegisterIndex > 15)
@@ -49,11 +76,19 @@ namespace YACE
             RegisterValueChanged?.Invoke();
         }
 
+        /// <summary>
+        /// Get the value of register I (address register).
+        /// </summary>
+        /// <returns></returns>
         public ushort GetRegisterI()
         {
             return _cpu.RegisterI;
         }
 
+        /// <summary>
+        /// Set the value of register I (address register).
+        /// </summary>
+        /// <param name="Value"></param>
         public void SetRegisterI(ushort Value)
         {
             if (Value > 0xFFF)
@@ -62,22 +97,37 @@ namespace YACE
             RegisterValueChanged?.Invoke();
         }
 
+        /// <summary>
+        /// Execute a single instruction on the CPU and then raise the stepped event.
+        /// </summary>
         public void Step()
         {
             _emulator.Tick();
             Stepped?.Invoke();
         }
         
+        /// <summary>
+        /// Gets a reference to the current main memory (RAM) of the emulator.
+        /// </summary>
+        /// <returns></returns>
         public byte[] GetRAM()
         {
             return _memory.RAM;
         }
 
+        /// <summary>
+        /// Gets a reference to the currently loaded ROM.
+        /// </summary>
+        /// <returns></returns>
         public Span<byte> GetROM()
         {
             return _memory.ReadROM();
         }
 
+        /// <summary>
+        /// Gets the start address of memory where ROMs are loaded.
+        /// </summary>
+        /// <returns></returns>
         public int GetROMBaseAddress()
         {
             return _memory.ROMBaseAddress;
